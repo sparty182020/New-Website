363f907039e1a031cffb30cb69419bf690c1f03d
Benjamin M <benpai@sparty18.com>
2023-04-18 10:53:45 -0400

    generalized updates

--- routes/index.js
+++ routes/index.js
@@ -1,22 +1,10 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/', (req, res, next) => {
-    res.render('main/index.pug', {
-        title: 'Home',
-        page: 1,
-        path: req.path
-    });
-});
+router.get('/', (req, res, _) => res.render('main/index.pug', {title: 'Home',page: 1,path: req.path,}));
 
-router.get('/socials', (req, res, next) => {
-    res.redirect('https://benpai.carrd.co');
-});
+router.get('/socials', (x, res, _) => res.redirect('https://benpai.carrd.co'));
 
-router.get('/blank', (req, res, next) => {
-    res.render('main/blank.pug', {
-        locked: true,
-    })
-});
+router.get('/blank', (x, res, _) => res.render('main/blank.pug', {locked: true,}));
 
 module.exports = router;
