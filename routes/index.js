ea5f21babd5df95dd23bc71228762918480e7649
Benjamin M <benpai@sparty18.com>
2023-04-21 13:38:53 -0400

    Additional Functions

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/', (req, res, _) => res.render('main/index.pug', {title: 'Home',page: 1,path: req.path,}));
+router.get('/', (req, res, _) => res.render('main/index.pug', { title: 'Home', page: 1, path: req.path, }));
 
 router.get('/socials', (x, res, _) => res.redirect('https://benpai.carrd.co'));
 
-router.get('/blank', (x, res, _) => res.render('main/blank.pug', {locked: true,}));
+router.get('/blank', (x, res, _) => res.render('main/blank.pug', { locked: true, }));
 
 module.exports = router;
